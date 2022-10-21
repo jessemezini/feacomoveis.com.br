2e1b40083631f4f0db54dcad53df263c2d2e6092
Jesse Mezini <jessemezini@gmail.com>
2022-10-21 14:42:33 -0300

    added logo

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Image from "next/image";
 
 import styles from "../styles/Banner.module.css";
 
 function Banner() {
-  return <div className={styles.container}>Banner</div>;
+  return (
+    <div className={styles.banner}>
+      <div className={styles.container}>
+        <header className={styles.navbar}>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </header>
+
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
+
+          <p className={styles.description}>
+            Get started by editing{" "}
+            <code className={styles.code}>pages/index.tsx</code>
+          </p>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
