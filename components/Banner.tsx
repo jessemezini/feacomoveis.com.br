dcece322a281aeb625904dd7d06c5a0648ffc7b1
Jesse Mezini <jessemezini@gmail.com>
2022-10-21 11:33:52 -0300

    added banner

---
+++ components/Banner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "../styles/Banner.module.css";
+
+function Banner() {
+  return <div className={styles.container}>Banner</div>;
+}
+
+export default Banner;
