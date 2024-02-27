874e907781f96bd43178916aa0a05a79f1942a21
Jesse Mezini <jessemezini@gmail.com>
2024-02-27 10:43:54 -0300

    using only needed font weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Oswald:wght@200&family=Source+Sans+3:ital,wght@1,800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Oswald:wght@200&family=Source+Sans+3:ital,wght@1,800&family=Ubuntu:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
