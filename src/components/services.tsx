d2faa712781c4ee2b087f21fcfeaddf883267e1b
Jesse Mezini <jessemezini@gmail.com>
2024-07-04 10:56:41 -0300

    services cards design ok

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,23 +1,46 @@
+import { MdOutlineDesignServices, MdTipsAndUpdates } from "react-icons/md";
+import { FaLaptopCode } from "react-icons/fa";
+
 export default function Services() {
   return (
-    <section className="bg-[#f7f7f7] -mt-32" data-theme="light">
-      <div className="container mx-auto pt-52">
-        <h2>Let’s see how it works</h2>
-      </div>
-      <div className="container mx-auto pt-52">
-        <h2>Let’s see how it works</h2>
-      </div>
-      <div className="container mx-auto pt-52">
-        <h2>Let’s see how it works</h2>
-      </div>
-      <div className="container mx-auto pt-52">
-        <h2>Let’s see how it works</h2>
-      </div>
-      <div className="container mx-auto pt-52">
-        <h2>Let’s see how it works</h2>
-      </div>
-      <div className="container mx-auto pt-52">
-        <h2>Let’s see how it works</h2>
+    <section className="bg-[#f7f7f7] -mt-32 pb-32" data-theme="light">
+      <div className="container mx-auto pt-64 text-center px-5">
+        <h2 className="max-w-3xl mx-auto text-3xl sm:text-4xl font-extrabold leading-normal sm:leading-normal tracking-tight mb-6">
+          Design & desenvolvimento de Sites e Aplicativos
+        </h2>
+
+        <div className="max-w-3xl mx-auto flex flex-col sm:flex-row gap-3 text-left [&>div]:prose [&>div]:py-10 [&>div]:px-6 [&>div]:flex-1 [&>div]:bg-secondary [&>div]:rounded-md [&>div]:shadow-sm">
+          <div data-theme="synthwave">
+            <MdOutlineDesignServices
+              size={44}
+              className="text-secondary-content"
+            />
+            <h3 className="!text-secondary-content">Design</h3>
+            <p className="font-light text-secondary-content">
+              Transformamos suas ideias em designs visualmente atraentes e
+              funcionais para sites e aplicativos.
+            </p>
+          </div>
+
+          <div data-theme="synthwave" className="text-secondary-content">
+            <FaLaptopCode size={44} className="text-secondary-content" />
+
+            <h3 className="!text-secondary-content">Desenvolvimento</h3>
+            <p className="font-light text-secondary-content">
+              Criamos soluções web e mobile de alta performance, adaptadas às
+              suas necessidades.
+            </p>
+          </div>
+
+          <div data-theme="synthwave" className="text-secondary-content">
+            <MdTipsAndUpdates size={44} className="text-secondary-content" />
+            <h3 className="!text-secondary-content">Manutenção</h3>
+            <p className="font-light text-secondary-content">
+              Garantimos que seu site ou aplicativo esteja sempre atualizado e
+              funcionando perfeitamente.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
