582e8c7cd8abf5704b71daaf0e1f400ec96d9798
Jesse Mezini <jessemezini@gmail.com>
2024-07-04 10:44:30 -0300

    upd seo and font weight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,14 @@ import { Manrope } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar";
 
-const font = Manrope({ weight: ["600", "500", "400"], subsets: ["latin"] });
+const font = Manrope({
+  weight: ["800", "600", "500", "400", "300"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "MZN App Lab",
-  description: "Criamos WebSites e Aplicativos",
+  description: "Agência de design e desenvolvimento de Sites e Aplicativos",
 };
 
 export default function RootLayout({
