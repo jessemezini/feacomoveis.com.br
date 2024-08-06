b4a184d1423072d20ad9ddacc02cfb487938ed6b
Jesse Mezini <jessemezini@gmail.com>
2024-08-06 11:18:07 -0300

    added new horizontal logo and new navbar design

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,31 +4,55 @@ import { FaWhatsapp } from "react-icons/fa";
 
 export default function NavBar() {
   return (
-    <div className="navbar bg-base-100 !container mx-auto mt-4">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="md:max-w-screen-md mx-auto md:shadow-md mt-5 md:rounded-lg">
+      <div className="navbar">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-base font-normal"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              <li>
+                <a href="/">Início</a>
+              </li>
+              <li>
+                <a href="#services">Serviços</a>
+              </li>
+              <li>
+                <a href="#contact">Contato</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-base font-normal"
-          >
+          <Link href="/">
+            <Image
+              src="/logo-horizontal.png"
+              alt="MZN App Lab"
+              width={150}
+              height={50.69}
+              priority
+            />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-base font-normal">
             <li>
-              <a href="/">Início</a>
+              <Link href="/">Início</Link>
             </li>
             <li>
               <a href="#services">Serviços</a>
@@ -38,36 +62,15 @@ export default function NavBar() {
             </li>
           </ul>
         </div>
-        <Link href="/">
-          <Image
-            src="/logo.png"
-            alt="MZN App Lab"
-            width={84}
-            height={84}
-            priority
-          />
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-base font-normal">
-          <li>
-            <Link href="/">Início</Link>
-          </li>
-          <li>
-            <a href="#services">Serviços</a>
-          </li>
-          <li>
-            <a href="#contact">Contato</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a
-          href="https://wa.me/5519983373120"
-          className="btn btn-secondary text-base"
-        >
-          WhatsApp <FaWhatsapp size="1.25rem" />
-        </a>
+        <div className="navbar-end">
+          <a
+            href="https://wa.me/5519983373120"
+            className="btn btn-secondary text-base"
+          >
+            <span className="max-[340px]:hidden">WhatsApp</span>{" "}
+            <FaWhatsapp size="1.25rem" />
+          </a>
+        </div>
       </div>
     </div>
   );
