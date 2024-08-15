08c993e38b435ec4abae1cea5c2003db8134c553
Jesse Mezini <jessemezini@gmail.com>
2024-08-15 14:24:53 -0300

    testimonials ok

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -6,39 +6,34 @@ import { Pagination } from "swiper/modules";
 
 const data = [
   {
-    text: "Eu tinha uma visão clara do que queria para o meu site, e o Jesse e sua equipe da MZN Digital superaram minhas expectativas. Eles foram extremamente profissionais, criativos e rápidos em entregar exatamente o que eu imaginava. A comunicação foi excelente, e o suporte durante todo o processo fez com que me sentisse em boas mãos. Recomendo de olhos fechados!",
-    name: "Nome do Cliente",
-    businessName: "Empresa do Cliente",
-  },
-  {
-    text: "Trabalhar com o Jesse Mezini e a equipe da MZN Digital foi uma experiência incrível. Desde o início, eles entenderam exatamente o que eu precisava e transformaram minhas ideias em um site/aplicativo funcional e esteticamente agradável. A dedicação ao projeto e a atenção aos detalhes realmente se destacaram. Estou extremamente satisfeito com o resultado e recomendo a MZN Digital para quem procura serviços de design e desenvolvimento de alta qualidade.",
+    text: "A MZN Digital fez um trabalho incrível no desenvolvimento do meu site. O Jesse foi muito prestativo durante todo o processo, explicando cada detalhe e certificando-se de que eu estava satisfeita em cada etapa. O resultado final foi além do que eu esperava. Definitivamente recomendaria a MZN para qualquer pessoa precisando de um site de qualidade.",
     name: "Érica Neves",
     businessName: "J3A Metais",
   },
   {
+    text: "Trabalhar com o Jesse Mezini e a equipe da MZN Digital foi uma experiência incrível. Desde o início, eles entenderam exatamente o que eu precisava e transformaram minhas ideias em um aplicativo funcional e esteticamente agradável. A dedicação ao projeto e a atenção aos detalhes realmente se destacaram. Estou extremamente satisfeito com o resultado e recomendo a MZN Digital para quem procura serviços de design e desenvolvimento de alta qualidade.",
+    name: "Fabio Lima",
+    businessName: "Meditar para Despertar",
+  },
+  {
+    text: "Eu tinha uma visão clara do que queria para o meu site, e o Jesse e sua equipe da MZN Digital superaram minhas expectativas. Eles foram extremamente profissionais, criativos e rápidos em entregar exatamente o que eu imaginava. A comunicação foi excelente, e o suporte durante todo o processo fez com que me sentisse em boas mãos. Recomendo o trabalho.",
+    name: "André Martinatti",
+    businessName: "Gazeta de Iracemápolis",
+  },
+  {
     text: "Trabalhar com a MZN Digital foi uma decisão acertada. O Jesse realmente se destacou ao compreender o meu negócio e aplicar isso no design do site. Fiquei impressionado com a capacidade deles de equilibrar funcionalidade e estilo. Desde o primeiro contato até o lançamento, todo o processo foi tranquilo e eficiente.",
-    name: "Nome do Cliente",
-    businessName: "Empresa do Cliente",
+    name: "Sebastião Cordeiro",
+    businessName: "Empório da Terra",
   },
   {
-    text: "Jesse e a equipe da MZN Digital foram fenomenais. Eles criaram um site que não só é lindo, mas também fácil de navegar e incrivelmente rápido. Desde o briefing até a entrega final, senti que estava em parceria com uma equipe dedicada e com grande conhecimento técnico. Estou muito satisfeito com o resultado!",
-    name: "Nome do Cliente",
-    businessName: "Empresa do Cliente",
+    text: "Jesse e a equipe da MZN Digital criaram um site que não só é lindo, mas também fácil de navegar e incrivelmente rápido. Desde o início até a entrega final, senti que estava em parceria com uma equipe dedicada. Estou muito satisfeito com o resultado!",
+    name: "Jhonatan Oliveira",
+    businessName: "Pizza da Casa",
   },
   {
-    text: "Trabalhar com a MZN Digital foi uma experiência gratificante. Eles souberam traduzir exatamente o que eu precisava em um site moderno e prático. O Jesse foi extremamente atencioso, sempre disponível para tirar dúvidas e fazer ajustes necessários. Com certeza voltarei a trabalhar com eles em projetos futuros.",
-    name: "Nome do Cliente",
-    businessName: "Empresa do Cliente",
-  },
-  {
-    text: "A MZN Digital fez um trabalho incrível no desenvolvimento do meu site. O Jesse foi muito paciente e prestativo durante todo o processo, explicando cada detalhe e certificando-se de que eu estava satisfeito em cada etapa. O resultado final foi além do que eu esperava. Definitivamente recomendaria a MZN para qualquer pessoa precisando de um site de qualidade.",
-    name: "Nome do Cliente",
-    businessName: "Empresa do Cliente",
-  },
-  {
-    text: "A MZN Digital entregou um trabalho impecável. A dedicação do Jesse e da sua equipe foi evidente desde o primeiro contato. Eles foram rápidos, profissionais e criativos, e entregaram um site que reflete perfeitamente a identidade da minha empresa. Não poderia estar mais satisfeito com o serviço!",
-    name: "Nome do Cliente",
-    businessName: "Empresa do Cliente",
+    text: "Temos nosso cardápio online desde 2016 e estamos muitos satisfeitos com o resultado e feedback de nossos clientes. Recomendo.",
+    name: "Renato Rizzo",
+    businessName: "Pastelaria do Gordão",
   },
 ];
 
@@ -46,7 +41,7 @@ export default function Testimonials() {
   return (
     <section className="my-20">
       <h2 className="text-center text-2xl leading-normal tracking-tight mb-10">
-        Depoimentos de nossos clientes
+        Depoimentos
       </h2>
       <div className="container mx-auto">
         <Swiper
@@ -69,7 +64,7 @@ export default function Testimonials() {
         >
           {data.map((item, index) => (
             <SwiperSlide key={index}>
-              <div className="bg-base-200/50 p-10 prose">
+              <div className="bg-base-200/50 p-9 prose">
                 <blockquote>{item.text}</blockquote>
                 <div className="mt-5">
                   <h4>{item.name}</h4>
