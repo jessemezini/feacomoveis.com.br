16d23f704080499de9bd16046c6fc05b05dc6e4b
Jesse Mezini <jessemezini@gmail.com>
2024-08-28 14:27:50 -0300

    added header and consts

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "MZN Digital";
+export const SITE_DESCRIPTION =
+  "Agência de Design e Desenvolvimento de Sites e Aplicativos";
