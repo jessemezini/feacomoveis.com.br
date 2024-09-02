d65d7931ed2b799b0cea53749ef7530afd72f79f
Jesse Mezini <jessemezini@gmail.com>
2024-09-02 15:46:38 -0300

    added container padding

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,15 @@ import defaultTheme from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['"Manrope Variable"', ...defaultTheme.fontFamily.sans],
