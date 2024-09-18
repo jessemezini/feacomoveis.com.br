f0ffbd275bfcf6682848b086af21a6a703aec0a0
Jesse Mezini <jessemezini@gmail.com>
2024-09-18 15:27:58 -0300

    added SEO

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "MZN Digital";
+export const SITE_TITLE =
+  "JL Móveis de Aço - Armários, estantes, arquivos, vitrines e roupeiros em aço de alta resistência";
 export const SITE_DESCRIPTION =
-  "Agência de Design e Desenvolvimento de Sites e Aplicativos";
+  "Qualidade e Durabilidade para seu Espaço. Soluções versáteis e robustas para empresas e residências, com foco em organização e segurança.";
