d8da9823d079676b89264969e07bb1ffc72af850
Jesse Mezini <jessemezini@gmail.com>
2024-09-20 10:39:56 -0300

    added produtos content config and first mdx product

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from "astro:content";
+
+const productsCollection = defineCollection({
+  type: "content",
+  schema: () =>
+    z.object({
+      title: z.string(),
+      category: z.string(),
+      height: z.string(),
+      width: z.string(),
+      depth: z.string(),
+      sheetWidth: z.string(),
+      items: z.string().array(),
+      description: z.string(),
+      heroImage: z.string().optional(),
+    }),
+});
+
+export const collections = { produtos: productsCollection };
