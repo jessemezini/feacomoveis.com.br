a3f30871066e59c6a038f29d3a63c30c4032cc9e
Jesse Mezini <jessemezini@gmail.com>
2024-09-20 09:52:09 -0300

    added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import icon from "astro-icon";
 
 import partytown from "@astrojs/partytown";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://mzndigital.com",
-  integrations: [tailwind(), icon(), partytown()],
+  integrations: [tailwind(), icon(), partytown(), react()],
 });
