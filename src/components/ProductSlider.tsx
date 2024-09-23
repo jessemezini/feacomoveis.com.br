2e3c715f313813981481a097c11ec35485b479d7
Jesse Mezini <jessemezini@gmail.com>
2024-09-23 10:12:50 -0300

    product page ok

--- src/components/ProductSlider.tsx
+++ src/components/ProductSlider.tsx
@@ -4,9 +4,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // import required modules
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 
 interface ProductSlider {
   images: string[];
@@ -16,10 +17,19 @@ interface ProductSlider {
 export default function ProductSlider({ images, alt }: ProductSlider) {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Pagination]}
+        pagination={{ clickable: true }}
+        className="mySwiper"
+      >
         {images.map((image) => (
           <SwiperSlide key={image}>
-            <img src={image} alt={alt} />
+            <img
+              src={image}
+              alt={alt}
+              className="object-contain h-[500px] mx-auto p-5 mb-1 sm:mb-5"
+            />
           </SwiperSlide>
         ))}
       </Swiper>
