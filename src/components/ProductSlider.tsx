51775d99fe13c43c404c594c7970d189fae148fe
Jesse Mezini <jessemezini@gmail.com>
2024-09-22 23:58:47 -0300

    testing image path

--- src/components/ProductSlider.tsx
+++ src/components/ProductSlider.tsx
@@ -8,19 +8,20 @@ import "swiper/css/navigation";
 // import required modules
 import { Navigation } from "swiper/modules";
 
-export default function App() {
+interface ProductSlider {
+  images: string[];
+  alt: string;
+}
+
+export default function ProductSlider({ images, alt }: ProductSlider) {
   return (
     <>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {images.map((image) => (
+          <SwiperSlide key={image}>
+            <img src={image} alt={alt} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
