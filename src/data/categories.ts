c251792b2aeedf8ee065958e81afbd6e99c9559b
Jesse Mezini <jessemezini@gmail.com>
2024-09-24 20:02:06 -0300

    swiper ok loading data from categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,31 +1,31 @@
 export const categories = [
   {
     name: "Armários de Ferramentas",
-    imageUrl: "../../src/assets/images/categories/armarios-de-ferramentas.png",
+    imageUrl: "/src/assets/images/categories/armarios-de-ferramentas.png",
   },
   {
     name: "Armários de Ferramentas Balcão",
     imageUrl:
-      "../../src/assets/images/categories/armarios-de-ferramentas-balcao.png",
+      "/src/assets/images/categories/armarios-de-ferramentas-balcao.png",
   },
   {
     name: "Armários",
-    imageUrl: "../../src/assets/images/categories/armarios.png",
+    imageUrl: "/src/assets/images/categories/armarios.png",
   },
   {
     name: "Estantes",
-    imageUrl: "../../src/assets/images/categories/estantes.png",
+    imageUrl: "/src/assets/images/categories/estantes.png",
   },
   {
     name: "Roupeiros GRP",
-    imageUrl: "../../src/assets/images/categories/roupeiros-grp.png",
+    imageUrl: "/src/assets/images/categories/roupeiros-grp.png",
   },
   {
     name: "Carrinhos Auxiliares",
-    imageUrl: "../../src/assets/images/categories/carrinhos-auxiliares.png",
+    imageUrl: "/src/assets/images/categories/carrinhos-auxiliares.png",
   },
   {
     name: "Suportes",
-    imageUrl: "../../src/assets/images/categories/suportes.png",
+    imageUrl: "/src/assets/images/categories/suportes.png",
   },
 ];
