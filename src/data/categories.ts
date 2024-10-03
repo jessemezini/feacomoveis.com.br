3363ac0e57d18da57d693d1269c001588bfbe0c4
Jesse Mezini <jessemezini@gmail.com>
2024-10-03 16:45:52 -0300

    added missing categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -13,16 +13,32 @@ export const categories = [
     imageUrl: "/src/assets/images/categories/armarios.png",
   },
   {
-    name: "Estantes",
-    imageUrl: "/src/assets/images/categories/estantes.png",
+    name: "Armários Vitrines",
+    imageUrl: "/src/assets/images/categories/armarios-vitrines.png",
   },
   {
-    name: "Roupeiros GRP",
-    imageUrl: "/src/assets/images/categories/roupeiros-grp.png",
+    name: "Arquivos",
+    imageUrl: "/src/assets/images/categories/arquivos.png",
+  },
+  {
+    name: "Bancadas",
+    imageUrl: "/src/assets/images/categories/bancadas.png",
   },
   {
     name: "Carrinhos Auxiliares",
     imageUrl: "/src/assets/images/categories/carrinhos-auxiliares.png",
+  },
+  {
+    name: "Estantes",
+    imageUrl: "/src/assets/images/categories/estantes.png",
+  },
+  {
+    name: "Estantes Gaveteiros",
+    imageUrl: "/src/assets/images/categories/estantes-gaveteiros.png",
+  },
+  {
+    name: "Roupeiros GRP",
+    imageUrl: "/src/assets/images/categories/roupeiros-grp.png",
   },
   {
     name: "Suportes",
