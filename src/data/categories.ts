d0511928100f4791465f958172d8fa3deb2b7e09
Jesse Mezini <jessemezini@gmail.com>
2025-01-31 10:44:45 -0300

    added categories (missing grp and others)

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,19 +3,14 @@ export const categories = [
     name: "Armários de Ferramentas",
     imageUrl: "/src/assets/images/categories/armarios-de-ferramentas.png",
   },
-  {
-    name: "Armários de Ferramentas Balcão",
-    imageUrl:
-      "/src/assets/images/categories/armarios-de-ferramentas-balcao.png",
-  },
-  {
-    name: "Armários",
-    imageUrl: "/src/assets/images/categories/armarios.png",
-  },
-  {
-    name: "Armários Vitrines",
-    imageUrl: "/src/assets/images/categories/armarios-vitrines.png",
-  },
+  // {
+  //   name: "Armários",
+  //   imageUrl: "/src/assets/images/categories/armarios.png",
+  // },
+  // {
+  //   name: "Armários Vitrines",
+  //   imageUrl: "/src/assets/images/categories/armarios-vitrines.png",
+  // },
   {
     name: "Arquivos",
     imageUrl: "/src/assets/images/categories/arquivos.png",
@@ -25,23 +20,23 @@ export const categories = [
     imageUrl: "/src/assets/images/categories/bancadas.png",
   },
   {
-    name: "Carrinhos Auxiliares",
-    imageUrl: "/src/assets/images/categories/carrinhos-auxiliares.png",
-  },
-  {
     name: "Estantes",
     imageUrl: "/src/assets/images/categories/estantes.png",
   },
   {
-    name: "Estantes Gaveteiros",
-    imageUrl: "/src/assets/images/categories/estantes-gaveteiros.png",
+    name: "Gaveteiros",
+    imageUrl: "/src/assets/images/categories/gaveteiros.png",
   },
   {
-    name: "Roupeiros GRP",
-    imageUrl: "/src/assets/images/categories/roupeiros-grp.png",
+    name: "Mesas e Aparadores",
+    imageUrl: "/src/assets/images/categories/mesas-e-aparadores.png",
   },
-  {
-    name: "Suportes",
-    imageUrl: "/src/assets/images/categories/suportes.png",
-  },
+  // {
+  //   name: "Estantes Gaveteiros",
+  //   imageUrl: "/src/assets/images/categories/estantes-gaveteiros.png",
+  // },
+  // {
+  //   name: "Roupeiros GRP",
+  //   imageUrl: "/src/assets/images/categories/roupeiros-grp.png",
+  // },
 ];
